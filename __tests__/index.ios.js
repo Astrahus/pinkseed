98bfbba33602d61b25061e3c830c70c5b98fccc9
Alisson Desandro Bovenzo <adbovenzo@gmail.com>
2017-05-11 21:55:21 -0300

    Remoção do AVA e de volta ao Jest
    O ava mostrou algumas incompatibilidade pois necessitava de varias libs que eram incompativeis com o react-native 44 pois precisa do react 15.x e o react native 44 usa o pre alpha 16.x

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -4,9 +4,8 @@ import Index from '../index.ios.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
-});
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <Index />
+    );
+  });
